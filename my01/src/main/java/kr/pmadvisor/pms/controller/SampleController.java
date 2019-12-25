2c0a854663f4edfcd8ff79be5fab971ae24b205d
최규진 <gyujin0450@naver.com>
2019-12-25 14:06:53 +0900

    (p131)@RequestParam 를 이용한 파라미터 수집과 변환

--- my01/src/main/java/kr/pmadvisor/pms/controller/SampleController.java
+++ my01/src/main/java/kr/pmadvisor/pms/controller/SampleController.java
@@ -4,6 +4,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ch.qos.logback.classic.Logger;
 import kr.pmadvisor.pms.domain.SampleDTO;
@@ -35,6 +36,16 @@ public class SampleController {
 		
 		return "ex01"; // /WEB-INF/views/ex01.jsp 호출
 		
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name,
+		@RequestParam("age") int age) {
+		
+			logger.info("name: " + name);
+			logger.info("age: " + name);
+					
+			return "ex01";		
 	}
 	
 	

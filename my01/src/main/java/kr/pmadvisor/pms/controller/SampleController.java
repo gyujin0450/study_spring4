6d0a86686f1f0d9150cf8e808188014e5559d335
최규진 <gyujin0450@naver.com>
2019-12-25 12:24:49 +0900

    (p128)@GetMapping,@PostMapping

--- my01/src/main/java/kr/pmadvisor/pms/controller/SampleController.java
+++ my01/src/main/java/kr/pmadvisor/pms/controller/SampleController.java
@@ -2,6 +2,7 @@ package kr.pmadvisor.pms.controller;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ch.qos.logback.classic.Logger;
@@ -12,10 +13,19 @@ public class SampleController {
 
 	private Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
 	
-	@RequestMapping("basic")
+	@RequestMapping("/basic")
 	public void basic() {
 		
 		logger.info("basic.......................");
 		
 	}
+	
+	@GetMapping("/basicGet")
+	public void  basicGet() {
+		
+		logger.info("basic get only................");
+		
+	}
+	
+	
 }
